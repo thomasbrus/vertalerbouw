a0f7c3d72e0b2b8ece952a31210353510d4737ae
Thomas Brus <thomas.brus@me.com>
2014-05-24 03:48:05 +0200

    Finished todo (364008fb8108e75449828bb73246724e04155c18)

--- vb/week5/triangle/AbstractSyntaxTrees/SingleCaseBranch.java
+++ vb/week5/triangle/AbstractSyntaxTrees/SingleCaseBranch.java
@@ -3,7 +3,6 @@ package vb.week5.triangle.AbstractSyntaxTrees;
 import vb.week5.triangle.SyntacticAnalyzer.SourcePosition;
 
 public class SingleCaseBranch extends CaseBranch {
-  // TODO: Add else branch as rightmost node
   public Command CE;
 
   public SingleCaseBranch(IntegerLiteral ilAST, Command cAST,
